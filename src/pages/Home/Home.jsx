f53c25ceaec9dff39e4cdbd46bdc98d09369b278
ewhite1999 <ewhite1999@gmail.com>
2022-03-04 14:33:29 +0000

    add home page

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <section>
+      <p>Hey, I'm Edward</p>
+      <p>an aspiring developer</p>
+    </section>
+  );
+};
+
+export default Home;
