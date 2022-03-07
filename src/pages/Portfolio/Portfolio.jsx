68ef8930a27b973a4ca2768d101ab487a35eca15
ewhite1999 <ewhite1999@gmail.com>
2022-03-07 17:02:59 +0000

    add links to portfolio sections

--- src/pages/Portfolio/Portfolio.jsx
+++ src/pages/Portfolio/Portfolio.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import PortfolioSection from "../../components/PortfolioSection";
-
+import { NavLink } from "react-router-dom";
 const Portfolio = () => {
   return (
     <section>
       <h2>Portfolio</h2>
+      <nav>
+        <NavLink to="/before-futureproof">Before futureproof</NavLink>
+        <NavLink to="/confessions-page">Confessions page</NavLink>
+        <NavLink to="/habit-tracker">Habit Tracker</NavLink>
+      </nav>
       <Routes>
         <Route path="/" element={<p>Click on a link to see more about it</p>} />
         <Route path=":projectId" element={<PortfolioSection />} />
