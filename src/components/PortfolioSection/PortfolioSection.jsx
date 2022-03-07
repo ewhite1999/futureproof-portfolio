4095d5414295dd25ce3d627a078ecab3a5a56d3e
ewhite1999 <ewhite1999@gmail.com>
2022-03-07 17:51:03 +0000

    load intro component

--- src/components/PortfolioSection/PortfolioSection.jsx
+++ src/components/PortfolioSection/PortfolioSection.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import Intro from "../Intro";
+import * as data from "./data.json";
 
 const PortfolioSection = () => {
   let { projectId } = useParams();
   projectId = projectId.replace(/\W+/g, "").toLowerCase();
-  return <p>Hello {projectId} page</p>;
+  const sectionData = data[projectId];
+  const { intro, learning, technologies, nexttime } = sectionData;
+  return (
+    <>
+      <Intro intro={intro} />
+    </>
+  );
 };
 
 export default PortfolioSection;
