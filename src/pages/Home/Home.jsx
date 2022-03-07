b6aa70d2a0753a87123b6661b8b25dfadfb2e2b3
ewhite1999 <ewhite1999@gmail.com>
2022-03-07 11:14:51 +0000

    homepage font-size

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,8 +3,12 @@ import React from "react";
 const Home = () => {
   return (
     <section className="min-h-screen flex flex-col justify-center items-center">
-      <h1>Hey, I'm Edward</h1>
-      <p>an aspiring developer</p>
+      <h1 className="font-bold text-4xl mb-1 sm:text-6xl sm:mb-2 2xl:text-8xl 2xl:mb-4">
+        Hey, I'm Edward
+      </h1>
+      <p className="text-2xl mt-1 sm:text-4xl sm:mt-2 2xl:text-5xl 2xl:mt-4">
+        an aspiring developer
+      </p>
     </section>
   );
 };
