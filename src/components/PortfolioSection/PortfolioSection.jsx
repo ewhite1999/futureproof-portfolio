081cb38693eb94e1064cf5b392757d47836deaf1
ewhite1999 <ewhite1999@gmail.com>
2022-03-07 18:05:18 +0000

    catch error when trying to view non existant page

--- src/components/PortfolioSection/PortfolioSection.jsx
+++ src/components/PortfolioSection/PortfolioSection.jsx
@@ -4,15 +4,25 @@ import Intro from "../Intro";
 import * as data from "./data.json";
 
 const PortfolioSection = () => {
-  let { projectId } = useParams();
-  projectId = projectId.replace(/\W+/g, "").toLowerCase();
-  const sectionData = data[projectId];
-  const { intro, learning, technologies, nexttime } = sectionData;
-  return (
-    <>
-      <Intro intro={intro} />
-    </>
-  );
+  try {
+    const { projectId } = useParams();
+    const sanitisedProjectId = projectId.replace(/\W+/g, "").toLowerCase();
+    const sectionData = data[sanitisedProjectId];
+    if (!sectionData) {
+      throw `I haven't done a project to do with a ${projectId
+        .replace(/\W+/g, " ")
+        .toLowerCase()
+        .trim()} yet!`;
+    }
+    const { intro, learning, technologies, nexttime } = sectionData;
+    return (
+      <>
+        <Intro intro={intro} />
+      </>
+    );
+  } catch (e) {
+    return <p>{e}</p>;
+  }
 };
 
 export default PortfolioSection;
