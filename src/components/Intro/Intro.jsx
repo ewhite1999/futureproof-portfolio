d0fd3860a1117bfb312ef34f5e70fd2d96b4b1c9
ewhite1999 <ewhite1999@gmail.com>
2022-03-10 16:00:54 +0000

    style intro component

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const Intro = (props) => {
-  return <p>{props.intro}</p>;
+  return <p className="max-w-prose">{props.intro}</p>;
 };
 
 Intro.propTypes = {
