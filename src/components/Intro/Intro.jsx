daeb1fcee8fdaefff2f5943ea127249dddb972eb
ewhite1999 <ewhite1999@gmail.com>
2022-03-10 09:01:18 +0000

    add intro prop types

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const Intro = (props) => {
   return <p>{props.intro}</p>;
+};
+
+Intro.propTypes = {
+  intro: PropTypes.string.isRequired,
 };
 
 export default Intro;
