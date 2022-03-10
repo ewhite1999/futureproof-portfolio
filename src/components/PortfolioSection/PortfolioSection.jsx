81b9e8e786014d264a011f6d1e0f97ac5705e906
ewhite1999 <ewhite1999@gmail.com>
2022-03-10 15:59:33 +0000

    auto load title

--- src/components/PortfolioSection/PortfolioSection.jsx
+++ src/components/PortfolioSection/PortfolioSection.jsx
@@ -15,9 +15,12 @@ const PortfolioSection = () => {
         .toLowerCase()
         .trim()} yet!`;
     }
-    const { intro, learning, technologies, nexttime } = sectionData;
+    const { title, intro, learning, technologies, nexttime } = sectionData;
     return (
-      <div className=" bg-white absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+      <div className=" bg-zinc-50 fixed top-0 w-full max-w-fit p-6 right-0 left-0 mx-auto leading-6 text-slate-600 font-light z-20">
+        <h3 className="text-4xl border-b-2 border-pink-600 pb-4 mb-4 text-slate-900 font-normal">
+          {title}
+        </h3>
         <Intro intro={intro} />
         <Learning learning={learning} />
       </div>
