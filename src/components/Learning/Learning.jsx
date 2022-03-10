d723a5f4fec59a5828c55de880fca7e095d3e3ef
ewhite1999 <ewhite1999@gmail.com>
2022-03-10 16:00:38 +0000

    style learning component

--- src/components/Learning/Learning.jsx
+++ src/components/Learning/Learning.jsx
@@ -3,10 +3,17 @@ import PropTypes from "prop-types";
 const Learning = (props) => {
   return (
     <>
-      <h3>Learning:</h3>
+      <h3 className="text-2xl  font-normal mt-5 mb-2 text-pink-600">
+        Learning:
+      </h3>
       <ul>
         {props.learning.map((i, index) => (
-          <li key={`learning ${index}`}>{i}</li>
+          <li
+            className="marker:text-pink-600 list-disc ml-4 mb-1"
+            key={`learning ${index}`}
+          >
+            {i}
+          </li>
         ))}
       </ul>
     </>
