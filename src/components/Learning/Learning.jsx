b2cdb476f530e172d5d95931d6fce21ea1bde1bb
ewhite1999 <ewhite1999@gmail.com>
2022-03-10 09:10:28 +0000

    add proptypes to learning component

--- src/components/Learning/Learning.jsx
+++ src/components/Learning/Learning.jsx
@@ -1,15 +1,19 @@
 import React from "react";
-
+import PropTypes from "prop-types";
 const Learning = (props) => {
   return (
     <>
       <h3>Learning:</h3>
       <ul>
-        {props.learning.map((i) => (
-          <li>{i}</li>
+        {props.learning.map((i, index) => (
+          <li key={`learning ${index}`}>{i}</li>
         ))}
       </ul>
     </>
   );
+};
+
+Learning.propTypes = {
+  learning: PropTypes.array.isRequired,
 };
 export default Learning;
