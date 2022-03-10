9d0337364020bfa5311d0d22620ac85d76a7adcf
ewhite1999 <ewhite1999@gmail.com>
2022-03-10 15:58:59 +0000

    add hidden div to grey out bg

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import * as Pages from "./pages";
 const App = () => {
   return (
     <>
+      <div
+        id="darkCover"
+        className="bg-black h-full left-0 fixed top-0 w-full z-10 opacity-0 transition-opacity invisible duration-300"
+      ></div>
       <Pages.Home></Pages.Home>
       <Pages.Portfolio></Pages.Portfolio>
     </>
