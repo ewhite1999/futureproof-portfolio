0dc9a9027d00f7f275c0a63859aad06360dde1a2
ewhite1999 <ewhite1999@gmail.com>
2022-03-10 08:34:14 +0000

    add learn more component

---
+++ src/components/LearnMore/LearnMore.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const LearnMore = (props) => {
+  return (
+    <div className=" opacity-0 aboutDiv absolute h-full w-full bg-white top-0 transition-opacity duration-500">
+      <h3>{props.title}</h3>
+      <button className="border-2 cursor-pointer px-4 py-2 font-extralight border-pink-600  hover:bg-transparent hover:bg-pink-600 hover:text-zinc-50 hover:transition-colors hover:duration-500 sm:text-xl 2xl:text-2xl">
+        Learn more.
+      </button>
+    </div>
+  );
+};
+
+export default LearnMore;
