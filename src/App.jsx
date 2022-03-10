f16e9a81d2edcc9ac826be667ffee2b719679a10
ewhite1999 <ewhite1999@gmail.com>
2022-03-10 16:31:06 +0000

    make DarkDiv component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import * as Pages from "./pages";
+import DarkDiv from "./components/DarkDiv";
 
 const App = () => {
   return (
     <>
-      <div
-        id="darkCover"
-        className="bg-black h-full left-0 fixed top-0 w-full z-10 opacity-0 transition-opacity invisible duration-300"
-      ></div>
+      <DarkDiv />
       <Pages.Home></Pages.Home>
       <Pages.Portfolio></Pages.Portfolio>
     </>
