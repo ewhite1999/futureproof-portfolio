7c6d082de43afc142aaa0ebc20d31d54d4a6a1b9
ewhite1999 <ewhite1999@gmail.com>
2022-03-11 11:21:14 +0000

    only toggle non current divs

--- src/components/PortfolioCard/PortfolioCard.jsx
+++ src/components/PortfolioCard/PortfolioCard.jsx
@@ -1,20 +1,20 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { isBrowser, isMobile } from "react-device-detect";
+import { isBrowser } from "react-device-detect";
 import LearnMore from "../LearnMore";
 const PortfolioCard = (props) => {
   const handleMouse = (e) => {
     // Hide all learn more sections
-    if (isMobile) {
-      document.querySelectorAll(".aboutDiv").forEach((div) => {
+    document.querySelectorAll(".aboutDiv").forEach((div) => {
+      if (!e.currentTarget.querySelector(".aboutDiv") === div) {
         div.classList.remove("opacity-0");
         div.classList.add("opacity-0");
-      });
-      document.querySelectorAll(".learnMoreBtn").forEach((btn) => {
-        btn.classList.remove("hidden");
-        btn.classList.add("hidden");
-      });
-    }
+      }
+    });
+    document.querySelectorAll(".learnMoreBtn").forEach((btn) => {
+      btn.classList.remove("hidden");
+      btn.classList.add("hidden");
+    });
     const btn = e.currentTarget.querySelector(".learnMoreBtn");
     e.currentTarget.querySelector(".aboutDiv").classList.toggle("opacity-0");
     btn.classList.toggle("hidden");
