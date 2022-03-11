32c93d3cbc72e1f347d65d18d494f3817ec55b05
ewhite1999 <ewhite1999@gmail.com>
2022-03-11 09:42:49 +0000

    render role

--- src/components/PortfolioSection/PortfolioSection.jsx
+++ src/components/PortfolioSection/PortfolioSection.jsx
@@ -4,6 +4,7 @@ import Btns from "../Btns";
 import Intro from "../Intro";
 import Learning from "../Learning";
 import NextTime from "../NextTime";
+import Role from "../Role";
 import Technologies from "../Technologies";
 import * as data from "./data.json";
 
@@ -18,18 +19,19 @@ const PortfolioSection = () => {
         .toLowerCase()
         .trim()} yet!`;
     }
-    const { title, intro, learning, technologies, nexttime, git, site } =
+    const { title, intro, learning, technologies, nexttime, git, site, role } =
       sectionData;
     return (
       <div className=" bg-zinc-50 fixed top-0 w-full max-w-fit p-6 right-0 left-0 mx-auto leading-6 text-slate-600 font-light z-20 overflow-y-auto h-full">
         <h3 className="text-4xl border-b-2 border-pink-600 pb-4 mb-4 text-slate-900 font-normal">
           {title}
         </h3>
-        <Intro intro={intro} />
-        <Learning learning={learning} />
-        <Technologies tech={technologies} />
-        <NextTime next={nexttime} />
-        <Btns git={git} site={site} />
+        {intro && <Intro intro={intro} />}
+        {role && <Role role={role} />}
+        {learning && <Learning learning={learning} />}
+        {technologies && <Technologies tech={technologies} />}
+        {nexttime && <NextTime next={nexttime} />}
+        {git && site && <Btns git={git} site={site} />}
       </div>
     );
   } catch (e) {
--- src/components/Role/Role.jsx
+++ src/components/Role/Role.jsx
@@ -3,11 +3,16 @@ import PropTypes from "prop-types";
 
 const Role = (props) => {
   const roleArr = props.role;
-  return roleArr.map((p, index) => (
-    <p key={`role ${index}`} className="max-w-prose">
-      {p}
-    </p>
-  ));
+  return (
+    <>
+      <h3 className="text-2xl font-normal mt-5 mb-2 text-pink-600">Role:</h3>
+      {roleArr.map((p, index) => (
+        <p key={`role ${index}`} className="max-w-prose mb-4">
+          {p}
+        </p>
+      ))}
+    </>
+  );
 };
 
 Role.propTypes = {
